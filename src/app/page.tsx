2f7d4246fa506356a0e543017709580b726b800b
grandbusta <bustajay30@gmail.com>
2024-01-15 04:35:33 +0100

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,153 @@
+'use client'
 import Image from 'next/image'
+import { JSX, SVGProps, useEffect, useState } from 'react'
+import Web3 from 'web3'
 
 export default function Home() {
+  const providerUrl =
+    'https://goerli.infura.io/v3/af12bb5a50ba4edeba26b0662c4184a8'
+  let web3 = new Web3(providerUrl)
+
+  const [walletConnected, setWalletConnected] = useState(false)
+  const [walletAddress, setWalletAddress] = useState('')
+  const [walletBalance, setWalletBalance] = useState('0.00')
+
+  const handleConnectWalllet = () => {
+    let provider = window.ethereum
+    if (typeof provider != 'undefined') {
+      console.log('Metamask connected')
+      provider
+        .request({ method: 'eth_requestAccounts' })
+        .then((accounts: any) => {
+          setWalletAddress(accounts[0])
+          setWalletConnected(true)
+          web3.eth.getBalance(accounts[0]).then((bal) => {
+            let val = web3.utils.fromWei(bal, 'ether')
+            let formattedVal = parseFloat(val).toFixed(4)
+            setWalletBalance(formattedVal)
+          })
+        })
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-slate-900'>
+      <div className='w-3/6'>
+        <div className='bg-[#a17914] py-4 flex items-center justify-center rounded-t-lg'>
+          <h1 className='text-3xl'>Cryat - Swap ETH to Naira</h1>
+        </div>
+        <div className='bg-slate-950 p-6 rounded-b-lg'>
+          {walletConnected ? (
+            <WalletDetails address={walletAddress} balance={walletBalance} />
+          ) : (
+            <button
+              className='bg-[#1ba34d] hover:bg-[#12803a] py-2 w-full rounded-lg flex items-center justify-center'
+              onClick={handleConnectWalllet}
+            >
+              <WalletIcon className='h-5 w-5 mr-2' />
+              Connect wallet
+            </button>
+          )}
+
+          <div className='pt-6'>
+            <label htmlFor='ethAmount'>Amount in ETH</label>
+            <input
+              type='number'
+              name='ethAmount'
+              className='w-full rounded-sm text-white py-1 px-2 mt-2 bg-slate-900'
+              id=''
+              placeholder='0.1'
             />
-          </a>
+          </div>
+          <div className='pt-6'>
+            <label htmlFor='fiatAmount'>Equivalent in Naira</label>
+            <input
+              type='number'
+              name='fiatAmount'
+              className='w-full rounded-sm text-white p-2 mt-2 bg-slate-900'
+              id=''
+              placeholder='250000'
+            />
+          </div>
+          <div className='pt-6'>
+            <label htmlFor='banks'>Select Bank</label>
+            <br />
+            <select
+              name='banks'
+              id='banks'
+              className='w-full rounded-sm text-white p-2 mt-2 bg-slate-900'
+              defaultValue={'default'}
+            >
+              <option value='default' className=''>
+                Select a bank
+              </option>
+              <option value='access' className=''>
+                Access Bank
+              </option>
+              <option value='opay' className=''>
+                Opay
+              </option>
+            </select>
+          </div>
+          <div className='pt-6'>
+            <label htmlFor='accountNumber'>Bank Account Number</label>
+            <input
+              type='number'
+              name='accountNumber'
+              className='w-full rounded-sm text-white p-2 mt-2 bg-slate-900'
+              id=''
+              placeholder='8921764523'
+            />
+          </div>
+          <button className='bg-[#2371ed] hover:bg-[#1855b8] py-2 mt-6 w-full rounded-lg flex items-center justify-center'>
+            Swap
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+  )
+}
+
+function WalletDetails({
+  address,
+  balance,
+}: {
+  address: string
+  balance: string
+}) {
+  const trimAddress = () => {
+    return `${address.substring(0, 7)}...${address.substring(
+      address.length - 5
+    )}`
+  }
+  return (
+    <div className='w-full flex flex-col items-center justify-center'>
+      <div className='bg-slate-900 py-2 px-4 mb-2 rounded-lg hover:cursor-pointer'>
+        {trimAddress()}
+      </div>
+      <h1 className='text-3xl'>{balance} ETH</h1>
+      <h2>#2,500,000</h2>
+    </div>
+  )
+}
+
+function WalletIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path d='M21 12V7H5a2 2 0 0 1 0-4h14v4' />
+      <path d='M3 5v14a2 2 0 0 0 2 2h16v-5' />
+      <path d='M18 12a2 2 0 0 0 0 4h4v-4Z' />
+    </svg>
   )
 }
